a6780ad76a885695babe5ecbcab26daabe312a95
Liza <foxorlis1@gmail.com>
2026-09-12 23:13:22 +0300

    задание 5

---
+++ 1_pr/Main5.java
@@ -0,0 +1,11 @@
+public class Main5 {
+    public static void main(String[] args){
+        for(int i = 0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+    
+}
+
+// javac Main5.java сначало компилировать
+// java Main5 one two three запуск
