76bfe19c700a20125cd1504062155d50aab6c4de
Liza <foxorlis1@gmail.com>
2026-09-12 23:22:56 +0300

    задание 6

---
+++ 1_pr/Main6.java
@@ -0,0 +1,9 @@
+public class Main6 {
+    public static void main(String[] args){
+        for(int i = 1; i<=10; i++){
+            System.out.println(i + ": " + 1.0/i);
+
+        }
+    }
+    
+}
