66a3beb52412fd544469071d79cab675f3c4f6aa
Liza <foxorlis1@gmail.com>
2026-09-12 23:38:44 +0300

    задание 7

---
+++ 1_pr/Main7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ввкди чило: ");
+        int n = sc.nextInt();
+
+        System.out.println("Факториал: " + fact(n));
+    }
+
+    static int fact(int n){
+        int res = 1;
+        for(int i = 1; i<=n; i++){
+            res *= i;
+        }
+        return res;
+    }
+    
+}
