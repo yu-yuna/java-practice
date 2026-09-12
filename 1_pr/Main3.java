5ca7aa65d2bfc7d44b0b683a4cd7b737b8e67c69
Liza <foxorlis1@gmail.com>
2026-09-12 22:33:25 +0300

    задание 3

--- 1_pr/Main3.java
+++ 1_pr/Main3.java
@@ -1,6 +1,14 @@
 public class Main3 {
     public static void main(String[] args){
         System.out.println("Практика 1");
+        int[] num = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for(int i = 0 ; i < num.length; i++){
+            sum+= num[i];
+        }
+        System.out.println(sum);
+        System.out.println((double) sum/(num.length));
+
     }
     
 }
